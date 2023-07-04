ffb4e825e53390d1a61cc36ce7e393b8c21b768a
root <root@localhost.localdomain>
2023-07-04 11:29:05 +0530

    fine

--- modules/cluster/main.tf
+++ modules/cluster/main.tf
@@ -26,7 +26,7 @@ resource "aws_eks_node_group" "backend" {
   instance_types = ["t2.small"]
   remote_access {
     ec2_ssh_key = "rtp-03"
-    source_security_group_ids = var.security_group_id
+    source_security_group_ids = [var.security_group_id]
   } 
   
   labels =  tomap({env = "dev"})
